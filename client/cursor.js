8dcc647121638dd3aff7ab7d502d439eb8d9b750
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2023-04-16 21:36:43 +0530

    Update cursor.js

--- client/cursor.js
+++ client/cursor.js
@@ -1,10 +1,10 @@
 let docTitle = document.title;
 window.addEventListener("blur", () => {
     document.title = "Come Back :("
-})
+});
 window.addEventListener("focus", () => {
     document.title = docTitle
-})
+});
 
 const cursor = document.querySelector(".cursor");
 var timeout;
